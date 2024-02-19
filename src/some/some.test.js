4062bf8c3aa42f53e626051dc85900aad1f65d75
bryan athallah <bryanathallah7@gmail.com>
2024-02-20 06:42:37 +0700

    some

--- src/some/some.test.js
+++ src/some/some.test.js
@@ -1,7 +1,6 @@
 const some = require('./some');
 describe('some', () => {
   it('some', () => {
-    const result = some();
-    expect(result).toBe(false);
+    expect(some()).toBe(false);
   });
 });
