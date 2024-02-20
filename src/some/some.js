8a01d265fc5b381304bd32ab485aea55f4c76680
bryan athallah <bryanathallah7@gmail.com>
2024-02-21 02:57:28 +0700

    initial commit

--- src/some/some.js
+++ src/some/some.js
@@ -1,5 +1,4 @@
 function some() {
   return true;
 }
-
 module.exports = some;
--- src/some/some.test.js
+++ src/some/some.test.js
@@ -1,6 +1,6 @@
 const some = require('./some');
 describe('some', () => {
   it('some', () => {
-    expect(some()).toBe(false);
+    expect(some()).toBe(true);
   });
 });
